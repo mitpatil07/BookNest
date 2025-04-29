a4d84933d50c953846a57de28a954f94c8be4d52
mitpatil07 <mitesh8767@gmail.com>
2025-04-29 10:49:56 +0530

    allow host

--- LibManage/settings.py
+++ LibManage/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-zl8gjrineek-usge!9q=o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', 'localhost', '127.0.0.1', '*']
 
 # Application definition
 INSTALLED_APPS = [
